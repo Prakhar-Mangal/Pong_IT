ba1321c3f673cbe468e87e69044c3fcae5a4caff
TheCBKM <rjoshi14899@gmail.com>
2018-12-21 20:40:30 +0530

    modified

---
+++ L7.js
@@ -0,0 +1,126 @@
+var c=0 ,t=0,gamma,p=0;;
+var slides=[];
+var ball,a=6,b=4,x=200,y=300;
+var slide1,slde2,slide3,socket;
+var pong,gameover;
+function preload(){
+  soundFormats('mp3', 'ogg');
+  pong = loadSound('pong.mp3');
+  gameover=loadSound('Gameover.mp3');
+}
+function setup(){
+  startUp();
+checkCookie('highScore');
+checkCookie('recentTime');
+  print(getCookie('recentTime'));
+
+    print('high-'+getCookie('highScore'));
+  createCanvas(100*w,100*h);
+  ball =new Ball(50*w,50*h,20*w/h);
+  slide1=new Bar(25*w,97*h,25*w,3*h);
+  slide2=new Bar(25*w,0,25*w,3*h);
+  slides.push(slide1);
+  slides.push(slide2);
+}
+
+function draw(){
+background(25);
+showScore(t);
+fill(125);
+stroke(500);
+textSize(50*w/h);
+text(p, 15*w,15*h);
+ball.create();
+i=0;
+slides.forEach(slide => {
+  slide.create();
+  if(!(checkPaddle(slide))){
+    if(slide.x>100*w){
+      slides.splice(i,1);
+  }
+    else{
+  slide.move(slide.x,slide.y);
+  slide.x+=2;}
+}
+  else{
+    
+    slide.move(x,slide.y);
+  }
+
+i++;
+});
+var xx=map(gamma,-30,30,0,windowWidth);
+xx=mouseX;
+x+=a;
+y+=b;
+if(checkBounceX(x,ball)){
+  pong.play();
+  a=-a;
+}
+if(checkBounceY(y,ball)){
+  showGameOver();
+  setCookie('recentTime',t,1);
+  setHScore(t);
+  c=1;
+  t=0;
+  gameover.play();
+  noLoop();
+}
+ball.move(xx,y);
+slides.forEach(slide => {
+if(collide(ball,slide)){
+	  b=-b;
+      if(b<=0){y=y-4*h}
+      if(b>0){y=y+4*h}
+      ball.updateColor(random(255),random(255),random(255));
+      pong.play();
+      if(checkPaddle(slide)){p++;}
+
+}
+});
+passLevel();
+
+}
+function passLevel(){
+  if(t>20){
+    if(p==10){
+    setCookie('level','L2',1);
+    levelPassed(1);
+    c=2
+     noLoop();   }
+     else {showGameOver();
+  setCookie('recentTime',t,1);
+  setHScore(t);
+  c=1;
+  t=0;
+  gameover.play();
+  noLoop();}
+  }
+  }
+function mouseClicked(){
+//window.location.href='hello.html';
+ if(c==1){
+  window.location.href='index.html';
+    c=0;
+
+  }
+  if(c==2){
+  window.location.href='index.html';
+   
+  }
+
+}
+setInterval(function(){
+  t++;
+  if(t%2==0){
+      //window.location.href='index.html';
+  sliden1=new  Bar(random(-10,-5)*w,random(30,70)*h,random(10,25)*w,3*h);
+  slides.push(sliden1);
+}
+},1000);
+window.addEventListener('deviceorientation', function(e)
+{
+  //alpha = e.alpha;
+ // beta = e.beta;
+  gamma = e.gamma;
+});
