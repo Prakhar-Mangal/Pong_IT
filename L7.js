f0b1c4dd81579b85821138386ab4971799856a54
TheCBKM <38382861+TheCBKM@users.noreply.github.com>
2018-12-23 12:20:54 +0530

    Add files via upload

--- L7.js
+++ L7.js
@@ -106,7 +106,7 @@ setInterval(function(){
   t++;
   
   if(t%5==0){
-
+resetSketch();
     slides.pop();
     slides.pop();
    slide3=new Bar(random(10,45)*w,random(60,80)*h,15*w,3*h);
@@ -122,7 +122,42 @@ setInterval(function(){
   }
 
 },1000);
+function resetSketch(){
+  //slide1=new Bar(slide1.x,slide1.y,100*w,3*h);
+  //slide2=new Bar(slide2.x,slide2.y,slide2.l*2,3*h);
+  if(n1<2){
+    n1++;
+  ball.r= ball.r*3/4;
+  slide1.l=slide1.l/1.5;
+  slide2.l=slide2.l/1.5;
 
+  }
+  if(n==0){
+    n=1;
+  if(a<0)
+  a=-6;
+  else
+    a=6;
+    if(b<0)
+    b=-6;
+    else
+      b=6;
+    }
+    else{
+      n=0;
+      if(a<0)
+      a=-5;
+      else
+        a=5;
+        if(b<0)
+        b=-5;
+        else
+          b=5;
+    }
+//ball =new Ball(ball.x,ball.y,15*w/h);
+
+
+}
 
 
 
